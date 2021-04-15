37e8b0f9a46f95ddf985f4d7abbf32592a25336b
Nicolas Brouard <nicolas.brouard@cerence.com>
2021-04-15 19:42:53 -0400

    Added swagger annotations

--- src/main/java/com/upgrade/interview/challenge/campsitereservation/rest/BookingController.java
+++ src/main/java/com/upgrade/interview/challenge/campsitereservation/rest/BookingController.java
@@ -24,10 +24,23 @@ import com.upgrade.interview.challenge.campsitereservation.exception.BadRequestE
 import com.upgrade.interview.challenge.campsitereservation.exception.BookingNotFoundException;
 import com.upgrade.interview.challenge.campsitereservation.persistence.BookingEntity;
 import com.upgrade.interview.challenge.campsitereservation.persistence.BookingService;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@OpenAPIDefinition(
+    externalDocs = @ExternalDocumentation(description = "https://github.com/nicolasbrouard/campsite-reservation"),
+    info = @Info(
+        title = "Campsite reservation",
+        description = "Back-end Tech Challenge: Campsite reservation REST API service",
+        contact = @Contact(email = "nicolas.brouard@gmail.com", name = "Nicolas Brouard")))
+@Tag(name = "Campsite reservation REST api")
 public class BookingController {
 
   @SuppressWarnings("java:S1075")
@@ -42,6 +55,7 @@ public class BookingController {
     this.bookingService = bookingService;
   }
 
+  @Operation(summary = "Get the list of all reservations.")
   @GetMapping(path = BASE_PATH)
   public Stream<Booking> getBookingList() {
     return bookingService.findAll()
@@ -49,6 +63,7 @@ public class BookingController {
         .map(Booking::createFrom);
   }
 
+  @Operation(summary = "Get the information of the reservation with the given id.")
   @GetMapping(path = BASE_PATH + "/{id}")
   public Booking getBooking(@PathVariable long id) {
     return bookingService.findById(id)
@@ -56,6 +71,7 @@ public class BookingController {
         .orElseThrow(() -> new BookingNotFoundException(id));
   }
 
+  @Operation(summary = "Get information of the availability of the campsite for a given date range with the default being 1 month.")
   @GetMapping(path = BASE_AVAILABLE_PATH)
   public List<LocalDate> getAvailabilitiesBetween(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                                                   @RequestParam(required = false) LocalDate start,
@@ -75,6 +91,7 @@ public class BookingController {
     return bookingService.getAvailabilities(start, end);
   }
 
+  @Operation(summary = "Reserve the campsite.")
   @PostMapping(path = BASE_PATH)
   public Booking addBooking(@Valid @RequestBody Booking booking) {
     try {
@@ -85,6 +102,7 @@ public class BookingController {
     }
   }
 
+  @Operation(summary = "Update the reservation with the given id.")
   @PutMapping(path = BASE_PATH + "/{id}")
   public Booking updateBooking(@PathVariable long id, @Valid @RequestBody Booking booking) {
     log.info("Update booking {} with {}", id, booking);
@@ -94,6 +112,7 @@ public class BookingController {
     return Booking.createFrom(bookingService.update(oldBookingEntity, newBookingEntity));
   }
 
+  @Operation(summary = "Cancel the reservation with the given id.")
   @DeleteMapping(path = BASE_PATH + "/{id}")
   public void deleteBooking(@PathVariable long id) {
     log.info("Delete booking {}", id);
